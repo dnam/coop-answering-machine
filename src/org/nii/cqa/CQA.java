914b1ecef549f3564b87fb50ab616425a7f987e9
namd88 <namd88@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-07-04 07:30:38 +0000

    - Eclipse CQA (Cooperative Query Answering) folder - CQA.java containing the main class.

---
+++ src/org/nii/cqa/CQA.java
@@ -0,0 +1,17 @@
+/**
+ * Author: 
+ * Org: NII
+ */
+package org.nii.cqa;
+
+public class CQA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
