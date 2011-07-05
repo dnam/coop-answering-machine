48d7e5644dd2974c28f435e517fd5ffcca8be0d9
maheen2002 <maheen2002@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-07-05 02:44:37 +0000

    Added the class Literal

---
+++ trunk/src/org/nii/cqa/base/Literal.java
@@ -0,0 +1,58 @@
+/**
+ * Author: Maheen Bakhtyar
+ * Description: Stores the unique ID, literal being negative or not and the parameters of the literal.  
+ * Contains methods to access literal details and fetching the name of the literal from the symbol table based on the ID. 
+ */
+
+package org.nii.cqa.base;
+
+import java.util.Vector;
+
+public class Literal {
+	private int id;
+	private Boolean neg;
+	private Vector<Integer> params = new Vector<Integer>();
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setNegative(Boolean neg) {
+		this.neg = neg;
+	}
+
+	public Boolean isNegative() {
+		return neg;
+	}
+
+
+
+
+	public void setParams(Vector<Integer> params) {
+		this.params = params;
+	}
+
+	public Vector<Integer> getParams() {
+		return params;
+	}
+
+
+
+	//returns the corresponding String name of the literal
+	public String toString() {
+
+		return SymTable.getSym(this.id);
+	}
+
+
+	public void toTPTP(String q) {
+		// to convert to TPTP format
+	}
+
+}
