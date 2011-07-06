d42943d7f527c4362537093c8799d8f59bd17c7e
namd88 <namd88@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-07-06 06:55:48 +0000

    - Add Comparable interface

--- trunk/src/org/nii/cqa/base/Query.java
+++ trunk/src/org/nii/cqa/base/Query.java
@@ -6,7 +6,7 @@ package org.nii.cqa.base;
 
 import java.util.Vector;
 
-public class Query {
+public class Query implements Comparable {
 	private Vector<Literal> query;
 
 	public Query() {
@@ -21,6 +21,21 @@ public class Query {
 	// Converts the query into a string. Now is just a fake class
 	public String toString() {
 		return "this query has " + query.size() + " literals";
+	}
+
+	/*
+	 * Returns 0 if the two queries are equal
+	 */
+	public int compareTo(Query otherQuery) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (o instanceof Query)
+			return compareTo((Query) o);
+		return 1;
 	}
 
 }
