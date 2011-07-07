43cd28f6dfa3f8efcc7ccf438f0858ae3e7e86ba
namd88 <namd88@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-07-07 08:20:56 +0000

    Implemention DC operator

--- src/org/nii/cqa/operators/OperatorDC.java
+++ src/org/nii/cqa/operators/OperatorDC.java
@@ -8,13 +8,36 @@ import org.nii.cqa.base.*;
 import java.util.*;
 
 public class OperatorDC {
-	public Set<Query> doOp(Set<Query> inSet) {
+	/**
+	 * Returns a set of queries after performing
+	 * DC upon every queries given in inSet
+	 * @param inSet the input set of queries
+	 */
+	public static Set<Query> doOp(Set<Query> inSet) {
 		Iterator<Query> it = inSet.iterator();
-		return null;
+		Set<Query> retSet = new TreeSet<Query>();
+		
+		while (it.hasNext()) {
+			retSet.addAll(dropQuery(it.next()));
+		}
+
+		return retSet;
 	}
 	
-	public Set<Query> dropQuery(Query q) {
-		return null;
+	/**
+	 * Returns the set of queries after applying
+	 * DC on each literal of q
+	 * @param q the query to drop
+	 * @return a set of queries
+	 */
+	public static Set<Query> dropQuery(Query q) {
+		Set<Query> retSet = new TreeSet<Query>();
+		
+		int n = q.size();
+		for (int i = 0; i < n; i++)
+			retSet.add(q.dropAt(i));
+		
+		return retSet;
 	}
 
 }
