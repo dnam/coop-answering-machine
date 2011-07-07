a2a1c5e11202e3819d35d5e91ae7e15705a64d46
namd88 <namd88@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-07-07 05:10:47 +0000

    - MOdified Literal class

--- src/org/nii/cqa/base/Literal.java
+++ src/org/nii/cqa/base/Literal.java
@@ -6,9 +6,10 @@
 
 package org.nii.cqa.base;
 
+import java.util.Map;
 import java.util.Vector;
 
-public class Literal {
+public class Literal implements Comparable<Literal> {
 	private int id;
 	private Boolean neg;
 	private Vector<Integer> params;
@@ -42,6 +43,10 @@ public class Literal {
 	public void setParamAt(int i, int value) {
 		this.params.add(i, value);
 	}
+	
+	/**
+	 * @return Vector<Integer> a copy of the integer vectors in the literal
+	 */
 	public Vector<Integer> getAllParams() {
 
 		return (Vector<Integer>) this.params.clone();
@@ -67,4 +72,53 @@ public class Literal {
 		// to convert to TPTP format
 	}
 
+	/**
+	 * Compares to literal. This is a relaxed comparison
+	 */
+	@Override
+	public int compareTo(Literal other) {
+		return (this.id - other.id);
+	}
+	
+	
+	/**
+	 * @param other the other literal to compare with
+	 * @param theta store the substitution rule for the query
+	 * 			q1 (which this object belongs to) and q2 (which
+	 * 				the "other" object belongs to)
+	 * 		such that (q1)(theta) = q2
+	 * Note that NEW SUBSTITUTION RULES will be added to THETA
+	 * if we detect new substitution.
+	 * @return true if the two literals are equivalent
+	 */
+	public boolean isEquivalent(Literal other, Map<Integer, Integer> theta) {
+		if (this.id != other.id)
+			return false;
+		
+		if (theta == null)
+			throw new NullPointerException("theta cannot be null");
+		
+		// assertion. for debugging
+		assert this.params.size() == other.params.size();
+		
+		int n = this.params.size();
+		for (int i = 0; i < n; i++) {
+			int elem1 = this.params.get(i);
+			int elem2 = other.params.get(i);
+			
+			SymType type1 = SymTable.getTypeID(elem1);
+			SymType type2 = SymTable.getTypeID(elem2);
+			
+			if (type1 != type2) // different types 
+				return false;
+			
+			if (type1 == SymType.CONSTANT)
+				return false; // const vs. vars, or const vs. const
+			
+			// Var vs. var. Check substitution rule.		
+			//TODO: implements it here
+		}
+		
+		return false;
+	}
 }
