9069c66bda569264421095d7c6c813d0304f9cf2
maheen2002 <maheen2002@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-07-08 07:29:23 +0000

    - fixed the comparison method of two queries  - dropAt() is fixed in this version  - code cleaned  		- with extra printing statements removed  		- extra comments removed

--- src/org/nii/cqa/base/Query.java
+++ src/org/nii/cqa/base/Query.java
@@ -16,113 +16,96 @@ public class Query implements Comparable<Query> {
 		sortedLiterals = new PriorityQueue<Literal>();
 	}
 
+	/**
+	 * @param literal to be added
+	 * Description: add a literal to the query
+	 */
 	public void add(Literal literal) {
 		sortedLiterals.add(literal);
 	}
 
+	/**
+	 * @param literal to be iterated
+	 * @return: Iterator to iterate the literal
+	 */
 	public Iterator<Literal> iterator() {
 		return sortedLiterals.iterator();
 	}
 
+	
+	/**
+	 * @param compares if two Queries are duplicate
+	 * @return the rank of query
+	 * description: return 0 means duplicate queries
+	 * 				 negative value means otherQuery(parameter query) is ranked higher
+	 * 				 positive value means the this query is ranked higher
+	 */
 	@Override
-	public int compareTo(Query otherQ) { // compares if two Queries are
-											// duplicate
-		/**
-		 * return 0 means duplicate queries -1 means otherQuery(parameter query)
-		 * is ranked higher 1 means this query is ranked higher
-		 */
+	public int compareTo(Query otherQ) { 
+
 		int rank = 0;
 		boolean eqFlag = true;
 
 		this.printQ();
 		otherQ.printQ();
-		// System.out.println(this.query.get(0).getParamAt(0));
-		// System.out.println(this.query.get(0).getID());
 
-		if (this.sortedLiterals.size() == otherQ.sortedLiterals.size()) // checks if # of literals
-														// in both queries same
+		if (this.sortedLiterals.size() == otherQ.sortedLiterals.size()) 		// checks if # of literals same
 		{
-			System.out.println("Qs have same size");
 			Iterator<Literal> it1 = this.sortedLiterals.iterator();
 			Iterator<Literal> it2 = otherQ.sortedLiterals.iterator();
-			while (it1.hasNext()) {
-				Literal l1 = it1.next();
-				Literal l2 = it2.next();
+			Literal l1 = null;
+			Literal l2 = null;
+			while (it1.hasNext()) 
+			{
+				l1 = it1.next();
+				l2 = it2.next();
 				Map<Integer, Integer> theta = new HashMap<Integer, Integer>();
-				if (l1.isEquivalent(l2, theta)) // checks if l1 and l2 are
-												// equivalent
+				if (l1.isEquivalent(l2, theta)) 								// checks if two literals area equivalent
 				{
 					eqFlag = true;
-					System.out.println("Literals are equal");
 
 				} else {
 					eqFlag = false;
 					break;
 				}
 			}
-			if (!eqFlag) // checks if Queries are not equivalent
+
+			if (!eqFlag) 														// checks when Queries are not equivalent
 			{
-				rank = 2; // to change the initial value from 0 which means
-							// equivalent. Making sure to change it to 1 or -1
-							// later
-				System.out.println("Not equivalent");
-				int sum1 = 0, sum2 = 0; // calculate sum of IDs of literals to
-										// rank
-				it1 = this.sortedLiterals.iterator();
-				it2 = otherQ.sortedLiterals.iterator();
-
-				while (it1.hasNext()) {
-					Literal l1 = it1.next();
-					Literal l2 = it2.next();
-
-					if (l1.isNegative())
-						sum1 -= l1.getID();
-					else
-						sum1 += l1.getID();
-
-					if (l2.isNegative())
-						sum2 -= l2.getID();
-					else
-						sum2 += l2.getID();
-
-				}
-
-				if (sum1 >= sum2)
-					rank = 1;
-				else
-					rank = -1;
+				rank = 2; 
+				rank = l1.compareTo(l2);
 			}
-		} else if (this.sortedLiterals.size() < otherQ.sortedLiterals.size()) // checks if # of
-															// literals in q1 <
-															// than that of q2
+		} else if (this.sortedLiterals.size() < otherQ.sortedLiterals.size()) // checks if # of literals in q1 < q2
 		{
-			System.out.println("first query is lower");
 			rank = -1;
-		} else if (this.sortedLiterals.size() > otherQ.sortedLiterals.size()) // checks if # of
-															// literals in q1 >
-															// than that of q2
+		} else if (this.sortedLiterals.size() > otherQ.sortedLiterals.size()) // checks if # of literals in q1 > q2
 		{
-			System.out.println("first query is higher");
 			rank = 1;
 		}
 
 		return rank;
 	}
 
+	/**
+	 * description: prints the query
+	 */
 	public void printQ() {
 		System.out.println("Query:");
 		Iterator<Literal> it = sortedLiterals.iterator();
 		while (it.hasNext()) {
 			Literal l = it.next();
-			// l.getAllParams() = All parameters of that single literal
+			
 			System.out.println("literal " + l.getID() + "(" + l.getAllParams()
 					+ ")");
 		}
 		System.out.println("---------------------------------------");
 	}
 
-	// @author: Nam Dang
-	// Converts the query into a string. Now is just a fake class
+	
+	/**
+	 * @author Nam Dang
+	 * description: Converts the query into a string. Now is just a fake class
+	 */
 	public String toString() {
 		return "this query has " + sortedLiterals.size() + " literals";
 	}
@@ -133,7 +116,7 @@ public class Query implements Comparable<Query> {
 	public int size() {
 		return sortedLiterals.size();
 	}
-	
+
 	/** 
 	 * @param i the index to drop the literal. Assume that i < query.size()
 	 * 			index starts from 0 to (size - 1);
@@ -144,12 +127,13 @@ public class Query implements Comparable<Query> {
 		Query q = new Query();
 		q.sortedLiterals.addAll(this.sortedLiterals);
 		Iterator<Literal> it = q.sortedLiterals.iterator();
+		Literal l = it.next();
 		while (i > 0) {
-			it.next();
+			l = it.next();
 			i--;
 		}
-		it.remove();
-		
+		boolean b = q.sortedLiterals.remove(l);
+		assert b;
 		return q;
 	}
 
