3756df7969aa7e02c896e9d738a24129698891ae
maheen2002 <maheen2002@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-07-08 03:50:23 +0000

    - added method in Query Class to check for duplicate queries

--- src/org/nii/cqa/base/Query.java
+++ src/org/nii/cqa/base/Query.java
@@ -1,66 +1,120 @@
-/*
- * @author: Maheen
- * Description:
+/**
+ * @author Maheen Bakhtyar
+ * Description: The Query Class 
  */
 package org.nii.cqa.base;
 
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Vector;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+
 
 public class Query implements Comparable<Query> {
-	private Vector<Literal> query;
+	private PriorityQueue<Literal> query;
 
 	public Query() {
-		query = new Vector<Literal>();
+		query = new PriorityQueue<Literal>();
 	}
 
 	public void add(Literal literal) {
 		query.add(literal);
 	}
-	
+
 	public Iterator<Literal> iterator() {
 		return query.iterator();
 	}
-	
+
 	@Override
-	public int compareTo(Query otherQ) {
-		// return 0 means equal. -1 means otherQuery is higher, 1 means this query is higher
-		
+	public int compareTo(Query otherQ) {  //compares if two Queries are duplicate
+		/** return 0 means duplicate queries
+		 *  -1 means otherQuery(parameter query) is ranked higher
+		 * 1 means this query is ranked higher
+		*/
 		int rank = 0;
+		boolean eqFlag = true;
 
 		this.printQ();
 		otherQ.printQ();
-//		System.out.println(this.query.get(0).getParamAt(0));
-//		System.out.println(this.query.get(0).getID());
-		
-		if(this.query.size() == otherQ.query.size())
+		//		System.out.println(this.query.get(0).getParamAt(0));
+		//		System.out.println(this.query.get(0).getID());
+
+		if(this.query.size() == otherQ.query.size())	//checks if # of literals in both queries same
 		{
 			System.out.println("Qs have same size");
-			
+			Iterator<Literal> it1 = this.query.iterator();
+			Iterator<Literal> it2 = otherQ.query.iterator();
+			while(it1.hasNext())
+			{
+				Literal l1 = it1.next();
+				Literal l2 = it2.next();
+				Map<Integer, Integer> theta = new HashMap<Integer, Integer>();
+				if(l1.isEquivalent(l2, theta))	//checks if l1 and l2 are equivalent
+				{
+					eqFlag = true;
+					System.out.println("Literals are equal");
+
+				}
+				else{
+					eqFlag = false;
+					break;
+				}
+			}
+			if(!eqFlag)			//checks if Queries are not equivalent
+			{
+				rank = 2;    //to change the initial value from 0 which means equivalent. Making sure to change it to 1 or -1 later
+				System.out.println("Not equivalent");
+				int sum1 = 0,sum2 = 0;					//calculate sum of IDs of literals to rank
+				it1 = this.query.iterator();
+				it2 = otherQ.query.iterator();
+				
+				while(it1.hasNext())
+				{
+					Literal l1 = it1.next();					
+					Literal l2 = it2.next();
+
+					if(l1.isNegative())
+						sum1 -= l1.getID();
+					else 
+						sum1 += l1.getID();
+
+					if(l2.isNegative())
+						sum2 -= l2.getID();
+					else 
+						sum2 += l2.getID();
+					
+				}
+				
+				if(sum1 >= sum2)
+					rank = 1;
+				else
+					rank = -1;
+			}
 		}
-		else if(this.query.size() < otherQ.query.size())
+		else if(this.query.size() < otherQ.query.size())	//checks if # of literals in q1 < than that of q2
 		{
 			System.out.println("first query is lower");
 			rank = -1;
 		}
-		else if(this.query.size() > otherQ.query.size())
+		else if(this.query.size() > otherQ.query.size())	//checks if # of literals in q1 > than that of q2
 		{
 			System.out.println("first query is higher");
 			rank = 1;
 		}
 
-		
 		return rank;
 	}
-	
+
 	public void printQ()
 	{
 		System.out.println("Query:");
-		for(int i = 0; i<query.size(); i++)
+		Iterator<Literal> it = query.iterator();
+		while(it.hasNext())
 		{
-			// query.get(i).getID() = Single Literal ID
-			// query.get(i).getAllParams() = All parameters of that single literal
-			System.out.println("literal " + query.get(i).getID()+ "(" + query.get(i).getAllParams() + ")");
+			Literal l = it.next();
+			// l.getAllParams() = All parameters of that single literal
+			System.out.println("literal " + l.getID()+ "(" + l.getAllParams() + ")");
 		}
 		System.out.println("---------------------------------------");
 	}
@@ -71,6 +125,6 @@ public class Query implements Comparable<Query> {
 		return "this query has " + query.size() + " literals";
 	}
 
-	
+
 
 }
