aa85556933aca70c618c2e3370791960eb0688e4
namd88 <namd88@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-07-08 04:45:55 +0000

    - Making Operator as base class for operators - All operator classes are static

---
+++ src/org/nii/cqa/operators/OperatorAI.java
@@ -0,0 +1,15 @@
+package org.nii.cqa.operators;
+/**
+ * @author Nam Dang, Maheen Bakhtyar
+ * A class with only one public method that performs AI operation
+ * upon a given set of Queries
+ */
+
+import java.util.Set;
+import org.nii.cqa.base.*;
+
+public class OperatorAI extends Operator {
+	public static Set<Query> run(Set<Query> query) {
+		return null;
+	}
+}
--- src/org/nii/cqa/operators/OperatorDC.java
+++ src/org/nii/cqa/operators/OperatorDC.java
@@ -7,35 +7,21 @@ package org.nii.cqa.operators;
 import org.nii.cqa.base.*;
 import java.util.*;
 
-public class OperatorDC {
-	/**
-	 * Returns a set of queries after performing
-	 * DC upon every queries given in inSet
-	 * @param inSet the input set of queries
-	 */
-	public static Set<Query> doOp(Set<Query> inSet) {
-		Iterator<Query> it = inSet.iterator();
-		Set<Query> retSet = new TreeSet<Query>();
-		
-		while (it.hasNext()) {
-			retSet.addAll(dropQuery(it.next()));
-		}
-
-		return retSet;
-	}
+public class OperatorDC extends Operator {
+	
 	
 	/**
 	 * Returns the set of queries after applying
 	 * DC on each literal of q
 	 * @param q the query to drop
-	 * @return a set of queries
+	 * @return set of queries
 	 */
-	public static Set<Query> dropQuery(Query q) {
+	private static Set<Query> perform(Query query) {
 		Set<Query> retSet = new TreeSet<Query>();
 		
-		int n = q.size();
+		int n = query.size();
 		for (int i = 0; i < n; i++)
-			retSet.add(q.dropAt(i));
+			retSet.add(query.dropAt(i));
 		
 		return retSet;
 	}
