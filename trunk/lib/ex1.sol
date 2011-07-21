36a01adc3483740e7648996b72732f0c64e21bc6
maheen2002 <maheen2002@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-07-21 09:11:06 +0000

    - answer.txt ex1.sol

--- trunk/lib/ex1.sol
+++ trunk/lib/ex1.sol
@@ -1,7 +1,6 @@
 cnf(c1, axiom, [ill(pete, sinusitis)]).
-cnf(c2, axiom, [ill(mary, sinusitis)]).
+cnf(c2, axiom, [ill(mary, bronchitis)]).
 cnf(c3, axiom, [-treat(pete, medi)]).
 cnf(c4, axiom, [ill(X, sinusitis), treat(X, medi)]).
-%cnf(query_clause, top_clause, [-ill(Y, sinusitis),-ill(Y, bronchitis), ans(Y)]).
-cnf(query_clause, top_clause, [--ill(Y, sinusitis), ans(Y)]).
+cnf(query_clause, top_clause, [-ill(X, sinusitis), ans(X)]).
 pf([ans(_)]).
