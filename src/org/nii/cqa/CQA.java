6d56615335a321ea2ebad65263bde9421c38a60e
namd88 <namd88@e5afabd7-63af-47f8-96d9-18c5fe425237>
2011-08-09 02:16:36 +0000

    - Update CQA with ExecutorService

--- src/org/nii/cqa/CQA.java
+++ src/org/nii/cqa/CQA.java
@@ -4,103 +4,133 @@
  */
 package org.nii.cqa;
 
+import java.io.*;
 import java.util.*;
 
+import org.nii.cqa.base.AnswerMap;
 import org.nii.cqa.base.KnowledgeBase;
 import org.nii.cqa.base.Query;
 import org.nii.cqa.base.QuerySet;
 import org.nii.cqa.operators.Operator;
+import org.nii.cqa.solar.SolarWorker;
+import org.nii.cqa.web.shared.WebAnswerMap;
+import org.nii.cqa.web.shared.WebQuerySet;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
-import javax.print.attribute.standard.Finishings;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
 
 public class CQA {
-	private static QuerySet root = new QuerySet(); 
+	private static QuerySet root = new QuerySet();
 
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		//The root of the tree
+		// The root of the tree
 		Query q = Query.parse("../CQA/lib/gen_query.txt");
 		root.add(q);
-		
+
 		// Initializes the static Knowledgebase
 		KnowledgeBase.init("../CQA/lib/gen_kb.txt");
-		
+
 		// A queue of QuerySet to process
 		Queue<QuerySet> workingQueue = new LinkedList<QuerySet>();
 		workingQueue.offer(root); // add the root
-		
+
 		// The thread to handle solar connection
 		SolarWorker worker = new SolarWorker();
-		Thread workerThread = new Thread(worker);
-		workerThread.start();
-		
+		FutureTask<AnswerMap> future = new FutureTask<AnswerMap>(worker);
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		executor.execute(future);
+
 		// Runs until the queue is empty
 		int cnt = 0; // TODO: configurable
-		while(!workingQueue.isEmpty()) {
-//			System.out.println("Size: " + workingQueue.size());
-			
+		while (!workingQueue.isEmpty()) {
+			// System.out.println("Size: " + workingQueue.size());
+
 			QuerySet nextSet = workingQueue.poll();
-			
+
 			Integer op = nextSet.getLastOp();
 			QuerySet ret = null;
-			
+
 			boolean doAI = false, doDC = false, doGR = false;
-			
+
 			// Root
 			if (op == null || op == Operator.GR_t) {
-				doAI = true; doDC = true; doGR = true;
-			}
-			else if (op == Operator.DC_t) {
-				doDC = true; doAI = true;
-			}
-			else if (op == Operator.AI_t) {
+				doAI = true;
+				doDC = true;
+				doGR = true;
+			} else if (op == Operator.DC_t) {
+				doDC = true;
+				doAI = true;
+			} else if (op == Operator.AI_t) {
 				doAI = true;
 			}
-			
+
 			if (doAI) {
 				ret = Operator.AI.run(nextSet);
 				if (ret != null) {
 					workingQueue.add(ret);
-					System.out.println(ret);
 					worker.queue(ret);
 					cnt += ret.size();
-					
+
 				}
 			}
-			
+
 			if (doDC) {
 				ret = Operator.DC.run(nextSet);
 				if (ret != null) {
 					workingQueue.add(ret);
 					worker.queue(ret);
 					cnt += ret.size();
-					System.out.println(ret);
 				}
 			}
-			
+
 			if (doGR) {
 				ret = Operator.GR.run(nextSet);
 				if (ret != null) {
 					workingQueue.add(ret);
 					worker.queue(ret);
 					cnt += ret.size();
-					System.out.println(ret);
 				}
 			}
 		}
-		
+
 		worker.setFinished(); // Notify the worker that we're done
+
+		System.out.println(root);
 		
-		workerThread.join();
 		
+		AnswerMap ans = future.get();
+
 		System.out.println("\n");
-		System.out.println("Answer set: " + worker.ansMap);
+		System.out.println("Answer set: " + ans.webConvert());
 		System.out.println("Done");
+		executor.shutdown();
+		
+		WebQuerySet newSet = new WebQuerySet();
+		try {
+			FileOutputStream fos = new FileOutputStream("test.tmp");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			WebQuerySet toWriteObj = root.webConvert();
+			toWriteObj.setAnsMap(ans.webConvert());
+			oos.writeObject(toWriteObj);
+
+			FileInputStream fin = new FileInputStream("test.tmp");
+			ObjectInputStream ois = new ObjectInputStream(fin);
+			newSet = (WebQuerySet) ois.readObject();
+		} catch (java.io.FileNotFoundException e) {
+			System.out.println(e.toString());
+		} catch (java.io.IOException e) {
+			System.out.println(e.toString());
+		}
+
+		
+		System.out.println(newSet);
+		
+		
 	}
+
 }
